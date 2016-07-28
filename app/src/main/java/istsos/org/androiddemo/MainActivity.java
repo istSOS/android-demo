ac33a43bc7b82c9173c6b941e46331daa93b5a4d
Florin Cioloboc <masterflorin@users.noreply.github.com>
2016-07-28 17:58:26 +0300

    Add DescribeSensor method and button

--- app/src/main/java/istsos/org/androiddemo/MainActivity.java
+++ app/src/main/java/istsos/org/androiddemo/MainActivity.java
@@ -1,7 +1,17 @@
 package istsos.org.androiddemo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import istsos.EventObject;
+import istsos.IstSOS;
+import istsos.IstSOSListener;
+import istsos.Procedure;
+import istsos.Server;
+import istsos.Service;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +19,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
+    public void describeSensor(View view){
+
+        IstSOS sos = IstSOS.getInstance();
+
+        String serverName = "localhost";
+        sos.initServer(serverName, "http://istsos.org/istsos/");
+
+        final Server server = sos.getServer(serverName);
+
+        server.loadServices(new IstSOSListener() {
+
+            @Override
+            public void onSuccess(EventObject event) {
+
+                Service service = server.getService("demo");
+
+                service.describeSensor("BELLINZONA", new IstSOSListener() {
+                    @Override
+                    public void onSuccess(EventObject event) {
+
+                        //store data from DescribeSensor
+                        Procedure procedure = (Procedure) event.getObject();
+                        //show data from procedure
+                        Intent showData = new Intent(Intent.ACTION_VIEW, Uri.parse(procedure.toString()));
+
+                        startActivity(showData);
+
+                    }
+
+                    @Override
+                    public void onError(EventObject event) {
+
+                    }
+                });
+            }
+
+
+            @Override
+            public void onError(EventObject event) {
+
+
+
+            }
+
+        });
+    }
 
 
 }
