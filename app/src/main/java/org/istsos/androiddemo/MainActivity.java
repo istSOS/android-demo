9ea84b64376af99ea8a4c3e54b7ab9f0d0a89cb5
Florin Cioloboc <masterflorin@users.noreply.github.com>
2016-08-06 23:56:44 +0300

    Add toolbar features, add dropdown menu for main activity

--- app/src/main/java/org/istsos/androiddemo/MainActivity.java
+++ app/src/main/java/org/istsos/androiddemo/MainActivity.java
@@ -9,7 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import org.istsos.client.EventObject;
 import org.istsos.client.IstSOS;
@@ -18,11 +23,15 @@ import org.istsos.client.Procedure;
 import org.istsos.client.Server;
 import org.istsos.client.Service;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, AdapterView.OnItemSelectedListener {
 
     private SensorManager mSensorManager;
     private Sensor mTemperature;
-
+    private ArrayList<Service> servicesLoadedInApp = null;
+    private ArrayAdapter<String> servicesArrayAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +41,85 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        //convert to array adapter type string
+        for (Service service : servicesLoadedInApp){
+
+            servicesArrayAdapter.add(service.getName());
+        }
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, (List<String>) servicesArrayAdapter);
+
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+        spinnerAdapter.notifyDataSetChanged();
+    }
+
+    public void loadServicesInApp(View view){
+
+        IstSOS sos = IstSOS.getInstance();
+
+        String serverName = "localhost";
+        sos.initServer(serverName, "http://istsos.org/istsos/");
+
+        final Server server = sos.getServer(serverName);
+
+        server.loadServices(new IstSOSListener() {
+            @Override
+            public void onSuccess(EventObject event) {
+
+                servicesLoadedInApp = (ArrayList<Service>)server.getServices();
+            }
+
+            @Override
+            public void onError(EventObject event) {
+
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -199,4 +281,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
 }
