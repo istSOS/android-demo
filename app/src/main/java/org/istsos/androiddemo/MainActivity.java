17e16200832762224eb5324431f55fae4a6254a9
Florin Cioloboc <masterflorin@users.noreply.github.com>
2016-08-06 18:18:31 +0300

    Add toolbar, new colors

--- app/src/main/java/org/istsos/androiddemo/MainActivity.java
+++ app/src/main/java/org/istsos/androiddemo/MainActivity.java
@@ -7,6 +7,8 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 import org.istsos.client.EventObject;
@@ -26,6 +28,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
 
