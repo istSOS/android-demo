cfb66ef1387d096825be387cb1d8537d47f7f45c
Florin Cioloboc <masterflorin@users.noreply.github.com>
2016-08-12 13:37:03 +0300

    Add method to get data from observation and display in a LineChart

--- app/src/main/java/org/istsos/androiddemo/DisplayGetObservation.java
+++ app/src/main/java/org/istsos/androiddemo/DisplayGetObservation.java
@@ -6,6 +6,8 @@ import android.view.View;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
 import org.istsos.client.EventObject;
 import org.istsos.client.IstSOS;
@@ -16,11 +18,13 @@ import org.istsos.client.Offering;
 import org.istsos.client.Procedure;
 import org.istsos.client.Server;
 import org.istsos.client.Service;
+import org.istsos.client.observation.DataArray;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -42,11 +46,50 @@ public class DisplayGetObservation extends AppCompatActivity{
             super.onCreate(savedInstanceState);
             setContentView(R.layout.get_observation);
 
+            //retrieve data from data array
+            DataArray dataArray = obsToDisplay.getResult().getDataArray();
+
+            ArrayList<String[]> values = dataArray.getValues();
+
+            //prepare the chart
             LineChart chart = (LineChart) findViewById(R.id.chart);
+            //initialize and store values to entries
+            List<Entry> entries = getEntries(values);
+            //assign entries to line data set
+            LineDataSet dataSet = new LineDataSet(entries, "Get Observation");
+            LineData lineData = new LineData(dataSet);
+            //set data to chart
+            chart.setData(lineData);
+
+        }
+
+
+        public List<Entry> getEntries(ArrayList<String[]> values){
 
             List<Entry> entries = new ArrayList<Entry>();
 
-            //ArrayList<> data = obsToDisplay.getResult().getDataArray();
+            for (String[] arrayValues : values){
+
+                String getX = arrayValues[0];
+                SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXX");
+                float formattedGetX = 0;
+
+                try {
+                    formattedGetX = sdformat.parse(getX).getTime();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                String[] yValues = Arrays.copyOfRange(arrayValues, 1, arrayValues.length);
+
+                for(String getY : yValues){
+
+                    float formattedGetY = Float.valueOf(getY);
+
+                    entries.add(new Entry(formattedGetX, formattedGetY));
+                }
+            }
+            return entries;
 
         }
 
