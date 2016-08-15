5c87fe19b68042f02c569dd63937ffebb448cd83
Florin Cioloboc <masterflorin@users.noreply.github.com>
2016-08-15 11:38:50 +0300

    Change structure for MainActivity, implement fragment for istsos services

--- app/src/main/java/org/istsos/androiddemo/MainActivity.java
+++ app/src/main/java/org/istsos/androiddemo/MainActivity.java
@@ -1,109 +1,27 @@
 package org.istsos.androiddemo;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-import org.istsos.client.EventObject;
-import org.istsos.client.IstSOS;
-import org.istsos.client.IstSOSListener;
-import org.istsos.client.Server;
-import org.istsos.client.Service;
-
-import java.util.ArrayList;
-
-
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-    private final String LOG_TAG = MainActivity.class.getSimpleName();
-
-    private ArrayList<Service> servicesLoadedInApp = new ArrayList<>();
-    Spinner spinner;
-    ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item);
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
-
-        spinner = (Spinner) findViewById(R.id.spinner);
-
-
-
-    }
-
-    public void loadServicesInApp(View view){
-
-        IstSOS sos = IstSOS.getInstance();
-
-        String serverName = "localhost";
-        sos.initServer(serverName, "http://istsos.org/istsos/");
-
-        final Server server = sos.getServer(serverName);
-
-        server.loadServices(new IstSOSListener() {
-            @Override
-            public void onSuccess(EventObject event) {
-
-                servicesLoadedInApp = (ArrayList<Service>)server.getServices();
-
-                spinnerAdapter.clear();
-                //convert to array adapter type string
-                for (Service service : servicesLoadedInApp){
-
-                    spinnerAdapter.add(service.getName());
-                }
-
-
-                spinner.setAdapter(spinnerAdapter);
-
-                //refresh adapter
-
-                spinnerAdapter.notifyDataSetChanged();
-
-            }
-
-            @Override
-            public void onError(EventObject event) {
-
-            }
-        });
-
-    }
-
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-
-        Intent intent = new Intent(MainActivity.this, DataActivities.class);
-        MainActivity.this.startActivity(intent);
-
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ServicesFragment())
+                    .commit();
+        }
     }
 
     @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -122,8 +40,4 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         return super.onOptionsItemSelected(item);
     }
-
-
-
-
 }
